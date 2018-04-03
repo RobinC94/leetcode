9c4e75bb270359670e184a5e20a08fd3eaa685a8
Robin Chen <leonharddt@sjtu.edu.cn>
2018-04-03 16:10:38 +0800

    Create pascalTriangle.cpp

---
+++ algorithms/cpp/pascalTriangle/pascalTriangle.cpp
@@ -0,0 +1,21 @@
+// Source : https://oj.leetcode.com/problems/pascals-triangle/
+// Author : Robi Chen
+// Date   : 2017.11.22
+
+/********************************************************************************** 
+* 
+* Given numRows, generate the first numRows of Pascal's triangle.
+* 
+* For example, given numRows = 5,
+* Return
+* 
+* [
+*      [1],
+*     [1,1],
+*    [1,2,1],
+*   [1,3,3,1],
+*  [1,4,6,4,1]
+* ]
+* 
+*               
+**********************************************************************************/
