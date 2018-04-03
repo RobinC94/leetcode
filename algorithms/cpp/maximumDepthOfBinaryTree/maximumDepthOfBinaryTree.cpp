60250075da770e5f6e163812fd78a86e92ca5a2a
Robin Chen <leonharddt@sjtu.edu.cn>
2018-04-03 16:12:08 +0800

    Create maximumDepthOfBinaryTree.cpp

---
+++ algorithms/cpp/maximumDepthOfBinaryTree/maximumDepthOfBinaryTree.cpp
@@ -0,0 +1,12 @@
+// Source : https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
+// Author : Robin Chen
+// Date   : 2017.11.22
+
+/********************************************************************************** 
+* 
+* Given a binary tree, find its maximum depth.
+* 
+* The maximum depth is the number of nodes along the longest path from the root node 
+* down to the farthest leaf node.
+*               
+**********************************************************************************/
