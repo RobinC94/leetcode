416d093b9d7ecfd02b5e8e8eca02e7241b3cdae7
RobinC94 <sissure@qq.com>
2018-04-05 22:38:49 +0800

    solve 19 20

--- algorithms/cpp/removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.cpp
+++ algorithms/cpp/removeNthNodeFromEndOfList/removeNthNodeFromEndOfList.cpp
@@ -1,6 +1,6 @@
 // Source : https://oj.leetcode.com/problems/remove-nth-node-from-end-of-list/
-// Author : 
-// Date   : 
+// Author : Robin Chen
+// Date   : 2018.4.5
 
 /********************************************************************************** 
 * 
@@ -14,7 +14,45 @@
 * 
 * Note:
 * Given n will always be valid.
-* Try to do this in one pass.
-* 
+* Try to do this in one pass. 
 *               
 **********************************************************************************/
+
+// 我的思路是先找到第n个node，然后从头和从node两路开搜
+// 到结尾的时候从头搜的就是要删掉的
+// 只不过要考虑很多边界情况
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        if(head -> next == NULL) return NULL;
+        int pos = 0;
+        ListNode* tmp = head;
+        ListNode* res = head;
+        while(pos < n){
+            tmp = tmp->next;
+            pos++;
+        }
+        if(tmp == NULL) return head->next;
+        while(tmp ->next != NULL){
+            tmp = tmp -> next;
+            res = res -> next;
+        }
+        tmp = res -> next;
+        res->next = tmp -> next;
+        delete(tmp);
+        return head;
+    }
+};
+
+// Status: Accepted
+// Runtime: 10 ms
+// 53.15%
--- algorithms/cpp/validParentheses/validParentheses.cpp
+++ algorithms/cpp/validParentheses/validParentheses.cpp
@@ -1,6 +1,6 @@
 // Source : https://oj.leetcode.com/problems/valid-parentheses/
-// Author : 
-// Date   : 
+// Author : Robin Chen
+// Date   : 2018.4.5
 
 /********************************************************************************** 
 * 
@@ -12,3 +12,46 @@
 * 
 *               
 **********************************************************************************/
+
+// 考的就是你记不记得栈的应用
+// 需要注意 一个是第一个字符就是后半括号
+// 另一个是最后栈非空
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i < s.length(); ++i)
+        {
+            char ch = s[i];
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+                if(ch==')'){
+                    if(st.top()!='(')
+                        return false;
+                    st.pop();
+                }
+                if(ch==']'){
+                    if(st.top()!='[')
+                        return false;
+                    st.pop();
+                }
+                if(ch=='}'){
+                    if(st.top()!='{')
+                        return false;
+                    st.pop();
+                }
+            }
+        }
+        if(st.empty())
+            return true;
+        else return false;
+    }
+};
+
+// Status: Accepted
+// Runtime: 4 ms
+// 87.93%
